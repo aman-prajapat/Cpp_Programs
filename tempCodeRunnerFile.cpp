681198b1624575db9c8c41ec20d92911d8dfb4d3
Aman Prajpat <98727830+aman-prajapat@users.noreply.github.com>
2023-03-03 22:14:41 +0530

    03/03/2023

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,48 @@
-else
+#include<iostream>
+using namespace std;
+
+class Birthday {
+    string name;
+    string dob;
+    Birthday *next = NULL;
+    public:
+    Birthday(){
+        cin.clear();
+        cin.ignore(1000, '\n');
+        getline(cin,name);
+        getline(cin,dob);
+    }
+    void assign(Birthday *temp);
+};
+
+void Birthday::assign(Birthday *temp){
+    next = temp;
+}
+int main()
+{
+    Birthday *instant;
+    int total_entries = 0;
+    int choice;
+    cout<<"Enter 1 for more friends entry: "<<endl;
+    cin>>choice;
+
+    label:
+    if(choice==1){
+        if(total_entries == 0){
+        instant = new Birthday;
+        }
+        else{
+             Birthday temp;
+            instant->assign(&temp);
+            total_entries++;
+
+        }
+    }
+    else{
+        cout<<"other";
+    }
+goto label;
+
+    
+    return 0;
+}
